07d660de7e724785b30d2831c5fcfdb57647a1ba
Fara-10 <haitova.farangis10@gmail.com>
2025-12-02 19:07:19 +0500

    add models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import  AbstractUser
+from .managers import CustomUserManager
+from random import randint
+import time
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=100, null =True, blank=True)
+    email = models.CharField(max_length=100, unique=True)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD= 'email'
+    REQUIRED_FIELDS = []
 
 # Create your models here.
--- server/settings.py
+++ server/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
+    'tajikistan.apps.TajikistanConfig',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,4 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
